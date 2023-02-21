6e9d59a48ec4b3438fc537dd74e59c20c975b782
ankit.mauni@marketwicks.com <bheemverma554@gmail.com>
2023-02-21 18:47:18 +0530

    13

--- app.js
+++ app.js
@@ -9,23 +9,51 @@ app.use(bodyParser.json());
 
 // handle incoming WhatsApp messages
 app.post('/webhook', (req, res) => {
-  const message = req.body.message;
+  const message = "vikas";
   const sender = req.body.sender;
+  // const message = req.body.message;
+  // const sender = req.body.sender;
 console.log("vikas")
   // do something with the incoming message and sender information
   console.log(`Received WhatsApp message from ${sender}: ${message}`);
 
- const  data1="${sender}: ${message}";
+//  const  data1="${sender}: ${message}";
 
-  const data = "sfdgdgfgfgfd"; 
-  fs.writeFileSync('https://wbhkapp.herokuapp.com/log.text', data); 
+//   const data = "sfdgdgfgfgfd"; 
+//   fs.writeFileSync('log.text', data); 
+
+
+
+// axios.post('https://watzapi.in/send-message', {
+//     "sender": 917428322239,
+//   "number": 917390982029,
+//   "message": "Hi there"
+// })
+
+axios.post('https://watzapi.in/send-message', {
+  "api_key":'2dv2PJ4X196rOeHM7sWN2CKFf3uy1I',
+  "sender": 919354869926,
+  "number": 917428322239,
+  "message": "Hi there1234"
+},
+
+)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
   res.sendStatus(200);
 
   
 });
 
-
+// start the server
+// app.listen(3000, () => {
+//   console.log('WhatsApp webhook server listening on port 3000');
+// });
 
 
 var server = app.listen(process.env.PORT || 3000, function () {
