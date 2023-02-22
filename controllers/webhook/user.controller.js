5690577adb3166fca74137e28fbfe12eb024c6ee
ankit.mauni@marketwicks.com <bheemverma554@gmail.com>
2023-02-22 17:19:08 +0530

    Reverting to <92a2b1a>

--- controllers/webhook/user.controller.js
+++ controllers/webhook/user.controller.js
@@ -2,83 +2,83 @@
 
 const saltRounds = 10;
 
-const axios = require('axios'); 
+const axios = require('axios');
 
 
 exports.sendMessage = async (req, res, next) => {
-var data={
-    
+    var data = {
+
         "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
         "sender": 919354869926,
-        "number":req.body.from,
-         "message": req.body.message,
+        "number": req.body.from,
+        "message": req.body.message,
         //  "message": 2,
-      
-}
-const obj=getDataObject(data)
-const url=getURL(data)
 
-   axios.post(url,obj )
-  .then(response => {
-    res.json({
-                        status: true,
-                        message: "Message deliver successfully"
-                    });
-  })
-  .catch(error => {
-   res.json(error)
-  });
-}
-
-
-function getURL(data1){
-
-    if(data1.message=='1'){
-        url="https://watzapi.in/send-message"
-    }else if(data1.message=='2'){
-       url=" https://watzapi.in/send-list"
-    
     }
-    else{
-       url="3"
+    const obj = getDataObject(data)
+    const url = getURL(data)
+
+    axios.post(url, obj)
+        .then(response => {
+            res.json({
+                status: true,
+                message: "Message deliver successfully"
+            });
+        })
+        .catch(error => {
+            res.json(error)
+        });
+}
+
+
+function getURL(data1) {
+
+    if (data1.message == '1') {
+        url = "https://watzapi.in/send-message"
+    } else if (data1.message == '2') {
+        url = " https://watzapi.in/send-list"
+
     }
-     return url
+    else {
+        url = "3"
+    }
+    return url
 
 }
 
-function getDataObject(data1){
+function getDataObject(data1) {
 
 
-    if(data1.message=='1'){
-        data1={
+    if (data1.message == '1') {
+        data1 = {
             "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
             "sender": 919354869926,
-            "number":data1.number,
+            "number": data1.number,
             "message": "Please Select the options",
-           
-                                                    
+
+
         }
-    }else if(data1.message=='2'){
-        data1={
+    } else if (data1.message == '2') {
+        data1 = {
             "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
             "sender": 919354869926,
-            "number":data1.number,
+            "number": data1.number,
             "footer": "ok",
             "message": "Please Select the options dsfsdf",
             "name": "Menu",
-             "title":"Account creation",
-             "list1":"list 1",
-             "list2":"option 2",
-             "list3":"option 3",
-             "list4":"option 4"
-                                                    
+            "title": "Account creation",
+            "list1": "list 1",
+            "list2": "option 2",
+            "list3": "option 3",
+            "list4": "option 4"
+
         }
-    
+
     }
-    else{
-        data1.message="3"
+    else {
+        data1.message = "3"
     }
-     return data1
-   
+    return data1
+
 
 }
