5baf31b1c07cae20edc4b8ba76fa536032dd6548
ankit.mauni@marketwicks.com <bheemverma554@gmail.com>
2023-02-23 12:06:16 +0530

    20

--- controllers/webhook/user.controller.js
+++ controllers/webhook/user.controller.js
@@ -1,63 +1,66 @@
 const saltRounds = 10;
 
-const axios = require('axios'); 
+const axios = require('axios');
 
 
 exports.sendMessage = async (req, res, next) => {
-    
-var data={
-    
+
+    var data = {
+
         "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
-        "sender": req.body.sender,
+        "sender": 919354869926,
         "number": req.body.number,
-         "message": req.body.message,
+        "message": req.body.message,
         //  "message": 2,
-      
-}
-const obj=getDataObject(data)
-const url=getURL(data)
 
-   axios.post(url,obj )
-  .then(response => {
-    res.json({
-                        status: true,
-                        message: "Message deliver successfully"
-                    });
-  })
-  .catch(error => {
-   res.json(error)
-  });
-}
-
-
-function getURL(data1){
-
-    if(data1.message=='1'){
-        url="https://watzapi.in/send-message"
-    }else if(data1.message=='2'){
-       url=" https://watzapi.in/send-list"
-    
     }
-    else{
-       url="3"
+    const obj = getDataObject(data)
+    const url = getURL(data)
+
+    axios.post(url, obj)
+        .then(response => {
+            res.json({
+                status: true,
+                message: "Message deliver successfully"
+            });
+        })
+        .catch(error => {
+            res.json(error)
+        });
+}
+
+
+function getURL(data1) {
+
+    if (data1.message == '1') {
+        url = "https://watzapi.in/send-message"
+    } else if (data1.message == '2') {
+        url = " https://watzapi.in/send-list"
+
+    }else if (data1.message == 'option1') {
+        url = " https://watzapi.in/send-list"
+
     }
-     return url
+    else {
+        url = "https://watzapi.in/send-message"
+    }
+    return url
 
 }
 
-function getDataObject(data1){
+function getDataObject(data1) {
 
 
-    if(data1.message=='1'){
-        data1={
+    if (data1.message == '1') {
+        data1 = {
             "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
             "sender": data1.sender,
-            "number":data1.number,
-            "message": "Wellcome to CRM. press 2 for our services",
-           
-                                                    
+            "number": data1.number,
+            "message": "Please Select the options",
+
+
         }
-    }else if(data1.message=='2'){
+    } else if (data1.message == '2') {
         data1={
             "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
             "sender": data1.sender,
@@ -72,35 +75,35 @@ function getDataObject(data1){
              "list4":"option4"
                                                     
         }
-        
-    
-    }else if(data1.message=='3'){
-        data1={
-            "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
-            "sender": data1.sender,
-            "number":data1.number,
-            "footer": "ok",
-            "message": "Please Select the options ",
-            "name": "Sub Menu",
-             "title":"Sub Menu",
-         
-             "list1":"option A",
-             "list2":"option B",
-             "list3":"option C"
-                                                    
-        }
+
     }
-    else{
-        data1={
-            "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
-            "sender": data1.sender,
-            "number":data1.number,
-            "message": "Contact to our support team",
-           
-                                                    
-        }
+    else if (data1.message == 'option1') {
+    data1={
+        "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
+        "sender": data1.sender,
+        "number":data1.number,
+        "footer": "ok",
+        "message": "Please Select the options ",
+        "name": "Menu",
+         "title":"Account creation",
+         "list1":"optionA",
+         "list2":"optionB",
+         "list3":"optionC",
+         "list4":"optionD"
+                                                
     }
-     return data1
-   
+  }
+  else{
+    data1 = {
+        "api_key": "2dv2PJ4X196rOeHM7sWN2CKFf3uy1I",
+        "sender": data1.sender,
+        "number": data1.number,
+        "message": "other",
+
+
+    }
+  }
+    return data1
+
 
 }
